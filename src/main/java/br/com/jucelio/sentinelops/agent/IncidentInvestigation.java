005222a7de255b5a3ad8877a9e1d66d752ca9d03
Jucelio Farias Coelho <juceliocoelho2022@gmail.com>
2026-09-22 10:02:22 -0300

    feat: expose policy evaluation in investigation result

--- src/main/java/br/com/jucelio/sentinelops/agent/IncidentInvestigation.java
+++ src/main/java/br/com/jucelio/sentinelops/agent/IncidentInvestigation.java
@@ -1,10 +1,11 @@
 package br.com.jucelio.sentinelops.agent;
 
+import br.com.jucelio.sentinelops.policy.PolicyEvaluation;
 import br.com.jucelio.sentinelops.security.SecurityFinding;
 
 public record IncidentInvestigation(
         InvestigationResult incidentAnalysis,
         SecurityFinding securityAnalysis,
-        boolean humanApprovalRequired
+        PolicyEvaluation policyEvaluation
 ) {
 }
